4d5572031f8a5ba9c0bce337124958459288b233
Ajeet Chaurasiya <cvajeetchaurasiya8887@gmail.com>
2026-02-09 18:42:57 +0530

    ...

---
+++ pattern/lefthalfpyramid.java
@@ -0,0 +1,15 @@
+
+public class lefthalfpyramid {
+  
+   public static void main(String[] var0) {
+      System.out.println("*");
+      System.out.println("* *");
+      System.out.println("* * *");
+      System.out.println("* * * *");
+      System.out.println("* * * * *");
+      // second method to print left half pyramid
+      System.out.println("*\n* *\n* * *\n* * * *\n* * * * *");
+   }
+}
+
+
