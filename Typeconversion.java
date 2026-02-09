2978b44fc04d1c3e8fa66cbcc9f34116534840f0
Ajeet Chaurasiya <cvajeetchaurasiya8887@gmail.com>
2026-02-09 16:27:10 +0530

    a..

---
+++ Typeconversion.java
@@ -0,0 +1,10 @@
+public class Typeconversion {
+   public static void main(String[] args) {
+    //implicit 
+    float myfloat=5;
+    System.out.println(myfloat);
+    //explicit
+    float num=(float)5d;
+    System.out.println(num);
+   } 
+}
